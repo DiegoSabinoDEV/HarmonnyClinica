2b26ffe944e6a633d1e620200e0ba0d1922352b6
Diego Gomes <diegosabinogomes@gmail.com>
2024-03-18 23:01:40 -0300

    Primeiro Upload so Site, verifcar uma configuração para o background do card incial

--- js/script.js
+++ js/script.js
@@ -30,3 +30,23 @@ const swiper = new Swiper(".swiper", {
 
   // Remova navigation e scrollbar se não estiverem no seu HTML
 });
+/*<script>
+// JavaScript para o menu de navegação dot
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('section');
+  const navDots = document.querySelector('.nav-dot-menu');
+
+  // Função para atualizar a navegação dot
+  function updateNavDots() {
+    let index = sections.length;
+
+    while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+    navDots.querySelectorAll('a').forEach((dot) => dot.classList.remove('active'));
+    navDots.querySelectorAll('a')[index].classList.add('active');
+  }
+
+  updateNavDots();
+  window.addEventListener('scroll', updateNavDots);
+});
+</script>*/
